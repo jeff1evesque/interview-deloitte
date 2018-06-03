abacc3defc3d85550e453266ac928e10d3d41dc8
jeffrey levesque <jeff1evesque@yahoo.com>
2018-06-03 15:46:11 -0400

    #24: chris_doe.R, add boxplot + violin visualizations

--- chris_doe.R
+++ chris_doe.R
@@ -133,3 +133,107 @@ data.f.adjusted$name <- tolower(data.f.adjusted$name)
 data.m.adjusted$name <- tolower(data.m.adjusted$name)
 data.f.complete$name <- tolower(data.f.complete$name)
 data.m.complete$name <- tolower(data.m.complete$name)
+
+##
+## chris doe's division: with adjusted anticipated values
+##
+row_adjusted <- which(data.m.adjusted$name == 'chris doe')
+column_division_adjusted <- which(colnames(data.m.adjusted) == 'division')
+column_net_time_adjusted <- which(colnames(data.m.adjusted) == 'net_time')
+division_adjusted_chris <- data.m.adjusted[row_adjusted, column_division_adjusted]
+time_adjusted_chris <- data.m.adjusted[row_adjusted, column_net_time_adjusted]
+
+## create subset dataframe
+data.m.adjusted.sub <- subset(data.m.adjusted[order(data.m.adjusted$net_time),], as.numeric(division) == division_chris, select = colnames(data.m.adjusted))
+data.m.complete.sub <- subset(data.m.adjusted[order(data.m.adjusted$net_time),], as.numeric(division) == division_chris, select = colnames(data.m.adjusted))
+
+## generate boxplot + violin
+color_scale <- c('darkblue', 'darkred', 'darkgreen', 'dodgerblue2', 'darkorchid')
+gg_full_adjusted <- ggplot(data.m.adjusted.sub, aes(x=factor(1), y=net_time)) +
+  geom_violin(trim=FALSE, fill='darkorchid4') +
+  geom_boxplot(width=0.25) +
+  geom_hline(yintercept=time_adjusted_chris, color='gray10', size=0.75, linetype='dashed') +
+  labs(x = 'State', y = 'Net Time (seconds)', title = 'Full Data: Net Time vs. State') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+gg_segregate_adjusted <- ggplot(data.m.adjusted.sub, aes(x=state, y=net_time, color=state)) +
+  geom_violin(trim=FALSE, aes(fill=state)) +
+  scale_fill_manual(values=color_scale) +
+  scale_color_manual(values=color_scale) +
+  geom_boxplot(width=0.75) +
+  geom_hline(yintercept=time_adjusted_chris, color='gray10', size=0.75, linetype='dashed') +
+  guides(color=FALSE) +
+  labs(x = 'State', y = 'Net Time (seconds)', title = 'Net Time vs State', fill = 'State') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+## save visualization
+png('visualization/10percentile-chris-adjusted.png', width = 1200, height = 600)
+
+## generate grid to contain visualization
+grid.arrange(
+  gg_full_adjusted,
+  gg_segregate_adjusted,
+  nrow = 1,
+  top = '10 Percentile vs Chris Doe: with adjusted anticipated values',
+  bottom = textGrob(
+    'Jeffrey Levesque',
+    gp = gpar(fontface = 3, fontsize = 9),
+    hjust = 1,
+    x = 1
+  )
+)
+
+## close current plot
+dev.off()
+
+##
+## chris doe's division: with empty values removed (not adjusted)
+##
+row_complete <- which(data.m.complete$name == 'chris doe')
+column_division_complete <- which(colnames(data.m.complete) == 'division')
+column_net_time_complete <- which(colnames(data.m.complete) == 'net_time')
+division_complete_chris <- data.m.complete[row_complete, column_division_complete]
+time_complete_chris <- data.m.complete[row_complete, column_net_time_complete]
+
+## create subset dataframe
+data.m.complete.sub <- subset(data.m.complete[order(data.m.complete$net_time),], as.numeric(division) == division_chris, select = colnames(data.m.complete))
+data.m.complete.sub <- subset(data.m.complete[order(data.m.complete$net_time),], as.numeric(division) == division_chris, select = colnames(data.m.complete))
+
+## generate boxplot + violin
+color_scale <- c('darkblue', 'darkred', 'darkgreen', 'dodgerblue2', 'darkorchid')
+gg_full_complete <- ggplot(data.m.complete.sub, aes(x=factor(1), y=net_time)) +
+  geom_violin(trim=FALSE, fill='darkorchid4') +
+  geom_boxplot(width=0.25) +
+  geom_hline(yintercept=time_complete_chris, color='gray10', size=0.75, linetype='dashed') +
+  labs(x = 'State', y = 'Net Time (seconds)', title = 'Full Data: Net Time vs. State') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+gg_segregate_complete <- ggplot(data.m.complete.sub, aes(x=state, y=net_time, color=state)) +
+  geom_violin(trim=FALSE, aes(fill=state)) +
+  scale_fill_manual(values=color_scale) +
+  scale_color_manual(values=color_scale) +
+  geom_boxplot(width=0.75) +
+  geom_hline(yintercept=time_complete_chris, color='gray10', size=0.75, linetype='dashed') +
+  guides(color=FALSE) +
+  labs(x = 'State', y = 'Net Time (seconds)', title = 'Net Time vs State', fill = 'State') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+## save visualization
+png('visualization/10percentile-chris-complete.png', width = 1200, height = 600)
+
+## generate grid to contain visualization
+grid.arrange(
+  gg_full_complete,
+  gg_segregate_complete,
+  nrow = 1,
+  top = '10 Percentile vs Chris Doe: with empty values removed (not adjusted)',
+  bottom = textGrob(
+    'Jeffrey Levesque',
+    gp = gpar(fontface = 3, fontsize = 9),
+    hjust = 1,
+    x = 1
+  )
+)
+
+## close current plot
+dev.off()
